0a3fe7ff0b66bcf67ac7d8fabc22332b8a3f457d
Sam Millington <s.j.millington@hotmail.co.uk>
2019-11-28 09:55:59 +0000

    adding mvc and introduction code (#1)

---
+++ mvc/controllers/users_controller.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/sjmillington/golang-microservices/mvc/utils"
+
+	"github.com/sjmillington/golang-microservices/mvc/services"
+)
+
+func GetUser(resp http.ResponseWriter, req *http.Request) {
+
+	userId, err := (strconv.ParseInt(req.URL.Query().Get("user_id"), 10, 64))
+	if err != nil {
+		userErr := &utils.ApplicationError{
+			Message:    "user_id must be a number",
+			StatusCode: http.StatusBadRequest,
+			Code:       "bad request",
+		}
+		resp.WriteHeader(userErr.StatusCode)
+		jsonValue, _ := json.Marshal(userErr)
+		resp.Write(jsonValue)
+		// Just return the Bad Request to the client
+		return
+	}
+
+	user, apErr := services.GetUser(userId)
+
+	if apErr != nil {
+
+		resp.WriteHeader(apErr.StatusCode)
+		jsonValue, _ := json.Marshal(apErr)
+		resp.Write(jsonValue)
+		// Handle the err and return to the client
+		return
+	}
+
+	//return user to client
+	jsonValue, _ := json.Marshal(user)
+	resp.Write(jsonValue)
+}
